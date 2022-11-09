a04476ed286566ef66cf8a7bd34d11c8c7fa3e15
nourez <nourez@nourez.dev>
2022-11-09 15:42:12 -0500

    Created CDK Code to deploy website to S3 Bucket

--- iac/lib/stateless-stack.ts
+++ iac/lib/stateless-stack.ts
@@ -1,16 +1,29 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 
 export class IacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a S3 bucket to host the static website
+    const MyWebsiteBucket = new s3.Bucket(this, "CloudResumeBucket", {
+      publicReadAccess: true,
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "404.html",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'IacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Deploy the static website content to the S3 bucket
+    const MyWebsiteBucketDeployment = new s3deploy.BucketDeployment(
+      this,
+      "CloudResumeBucketDeployment",
+      {
+        sources: [s3deploy.Source.asset("../frontend")],
+        destinationBucket: MyWebsiteBucket,
+      }
+    );
   }
 }
