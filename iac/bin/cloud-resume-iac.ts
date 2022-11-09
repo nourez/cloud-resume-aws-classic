81814a83253ee1a6a97284bd229b32ba7b2c8bcc
nourez <nourez@nourez.dev>
2022-11-09 17:30:14 -0500

    Added a cloudfront distribution

--- iac/bin/cloud-resume-iac.ts
+++ iac/bin/cloud-resume-iac.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { IacStack } from "../lib/stateless-stack";
+import { StatelessStack } from "../lib/stateless-stack";
 
 const app = new cdk.App();
-new IacStack(app, "IacStack", {
+new StatelessStack(app, "StatelessStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- iac/lib/stateless-stack.ts
+++ iac/lib/stateless-stack.ts
@@ -2,13 +2,14 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
-
-export class IacStack extends cdk.Stack {
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+export class StatelessStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Create a S3 bucket to host the static website
-    const MyWebsiteBucket = new s3.Bucket(this, "CloudResumeBucket", {
+    const CloudResumeBucket = new s3.Bucket(this, "CloudResumeBucket", {
       publicReadAccess: true,
       autoDeleteObjects: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
@@ -17,13 +18,41 @@ export class IacStack extends cdk.Stack {
     });
 
     // Deploy the static website content to the S3 bucket
-    const MyWebsiteBucketDeployment = new s3deploy.BucketDeployment(
+    const CloudResumeDeployment = new s3deploy.BucketDeployment(
       this,
       "CloudResumeBucketDeployment",
       {
         sources: [s3deploy.Source.asset("../frontend")],
-        destinationBucket: MyWebsiteBucket,
+        destinationBucket: CloudResumeBucket,
       }
     );
+
+    // Output the website URL
+    new cdk.CfnOutput(this, "CloudResumeWebsiteURL", {
+      value: CloudResumeBucket.bucketWebsiteUrl,
+    });
+
+    // Output if the bucket is a website (should be replaced with a test)
+    new cdk.CfnOutput(this, "CloudResumeWebsiteIsWebsite", {
+      value: CloudResumeBucket.isWebsite ? "Yes" : "No",
+    });
+
+    // Create a CloudFront distribution to serve the static website
+    const CloudResumeDistribution = new cloudfront.Distribution(
+      this,
+      "CloudResumeDistribution",
+      {
+        defaultBehavior: {
+          origin: new origins.S3Origin(CloudResumeBucket),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+      }
+    );
+
+    // Output the CloudFront distribution URL
+    new cdk.CfnOutput(this, "CloudResumeDistributionURL", {
+      value: CloudResumeDistribution.distributionDomainName,
+    });
   }
 }
